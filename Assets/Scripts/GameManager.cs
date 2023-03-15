41e7d09c1865094e59fb25a770c169e08e05d58b
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-15 18:44:48 +0200

    Added some buttons to modify Tops

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
@@ -9,12 +10,30 @@ public class GameManager : MonoBehaviour
     private GameObject top2;
     [SerializeField] private GameObject topPrefab;
 
+    [Header("UI")]
+    [SerializeField] private Slider top1SizeSlider;
+    [SerializeField] private Slider top2SizeSlider;
+    [SerializeField] private GameObject top1Wrapper;
+    [SerializeField] private GameObject top2Wrapper;
+    [SerializeField] private Button releaseTopsButton;
+    [SerializeField] private Button spawnNewTopsButton;
+
+    [Header("Misc")]
+    [SerializeField] private Material blueTopMaterial;
+    [SerializeField] private Material redTopMaterial;
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Home))
         {
             ReloadScene();
         }
+
+        top1Wrapper.SetActive(top1 != null);
+        top2Wrapper.SetActive(top2 != null);
+
+        releaseTopsButton.interactable = top1 != null && top2 != null;
+        spawnNewTopsButton.interactable = top1 == null || top2 == null;
     }
 
     public void ReloadScene()
@@ -35,7 +54,7 @@ public class GameManager : MonoBehaviour
             else if (!top2)
             {
                 top2 = Instantiate(topPrefab, arena.transform.Find("Spawn2").position, Quaternion.identity);
-                DisableTop(top1);
+                DisableTop(top2);
             }
         }
     }
@@ -68,5 +87,55 @@ public class GameManager : MonoBehaviour
         top.GetComponent<Rigidbody>().useGravity = false;
         top.transform.Find("TopWrapper").GetComponent<Top>().enabled = false;
         top.transform.rotation = Quaternion.identity;
+    }
+
+    public void ChangeTop_1_Size()
+    {
+        if (top1)
+        {
+            ChangeTopSize(top1, top1SizeSlider);
+        }
+    }
+
+    public void ToggleTop_1_Color()
+    {
+        if (top1)
+        {
+            ChangeTopColor(top1);
+        }
+    }
+
+    public void ChangeTop_2_Size()
+    {
+        if (top2)
+        {
+            ChangeTopSize(top2, top2SizeSlider);
+        }
+    }
+
+    public void ToggleTop_2_Color()
+    {
+        if (top2)
+        {
+            ChangeTopColor(top2);
+        }
+    }
+
+    private void ChangeTopSize(GameObject top, Slider slider)
+    {
+        top.transform.localScale = Vector3.one * slider.value;
+    }
+
+    private void ChangeTopColor(GameObject top)
+    {
+        Renderer topMeshRenderer = top.transform.Find("TopWrapper").Find("top").GetComponent<Renderer>();
+        if (topMeshRenderer.materials[1] == blueTopMaterial)
+        {
+            topMeshRenderer.materials[1] = redTopMaterial;
+        }
+        else
+        {
+            topMeshRenderer.materials[1] = blueTopMaterial;
+        }
     }
 }
