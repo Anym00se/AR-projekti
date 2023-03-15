d4050553b68e16ecdd201408c606444f70259da0
Anym00se <39912156+Anym00se@users.noreply.github.com>
2023-03-15 16:29:37 +0200

    Implemented spawning tops in the Arena

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    private GameObject top1;
+    private GameObject top2;
+    [SerializeField] private GameObject topPrefab;
 
     void Update()
     {
@@ -17,5 +20,40 @@ public class GameManager : MonoBehaviour
     public void ReloadScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SpawnTop()
+    {
+        GameObject arena = FindArena();
+        if (arena)
+        {
+            if (!top1)
+            {
+                top1 = Instantiate(topPrefab, arena.transform.Find("Spawn1").position, Quaternion.identity);
+                top1.GetComponent<Rigidbody>().useGravity = false;
+            }
+            else if (!top2)
+            {
+                top2 = Instantiate(topPrefab, arena.transform.Find("Spawn2").position, Quaternion.identity);
+                top2.GetComponent<Rigidbody>().useGravity = false;
+            }
+        }
+    }
+
+    public void ReleaseTops()
+    {
+        if (top1)
+        {
+            top1.GetComponent<Rigidbody>().useGravity = true;
+        }
+        if (top2)
+        {
+            top2.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    GameObject FindArena()
+    {
+        return GameObject.FindWithTag("Arena");
     }
 }
--- Assets/Scripts/PlaceOnPlane.cs
+++ Assets/Scripts/PlaceOnPlane.cs
@@ -8,7 +8,7 @@ using UnityEngine.XR.ARSubsystems;
 [RequireComponent(typeof(ARPlaneManager))]
 public class PlaceOnPlane : MonoBehaviour
 {
-
+    private List<ARPlane> arPlanes = new List<ARPlane>();
     private ARPlaneManager planeManager;
     public GameObject arenaPlanePrefab;
     public GameObject topPrefab;
@@ -33,14 +33,27 @@ public class PlaceOnPlane : MonoBehaviour
         planeManager.planesChanged -= OnPlanesChanged;
     }
 
+    void Update()
+    {
+        return;
+        // Destroy additional planes over a few frames
+        if (arPlanes.Count > 1)
+        {
+            Destroy(arPlanes[arPlanes.Count - 1]);
+        }
+    }
+
     void OnPlanesChanged(ARPlanesChangedEventArgs eventArgs)
     {
+        return;
         foreach(var trackedPlane in eventArgs.added)
         {
-            if (!arena)
-            {
-                arena = Instantiate(planeManager.planePrefab, trackedPlane.transform);
-            }
+            arPlanes.Add(trackedPlane);
+        }
+
+        foreach(var trackedPlane in eventArgs.removed)
+        {
+            arPlanes.Remove(trackedPlane);
         }
     }
 }
